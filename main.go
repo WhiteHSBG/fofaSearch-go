96fe9d33c3134038a363d4baecfc7f092bc42bbd
White <>
2021-05-30 01:56:54 +0800

    修改配置文件检测逻辑

--- main.go
+++ main.go
@@ -6,26 +6,22 @@ import (
 	"os"
 )
 
-
-
-
 func main() {
-	qu:=flag.String("query","","查询语句参数，建议使用''包起来")
-	outPut:=flag.String("output","","输出文件名")
+	tools.ChickConfig()
+	qu := flag.String("query", "", "查询语句参数，建议使用''包起来")
+	outPut := flag.String("output", "", "输出文件名")
 	flag.Parse()
 	if *qu == "" {
 		flag.Usage()
 		os.Exit(0)
 	}
 
-	tools.ChickConfig()
 	tools.NewConf("./config.yaml")
-	fofa:=tools.NewFofa()
+	fofa := tools.NewFofa()
 	fofa.Query(*qu)
-	checker:=tools.NewChecker(&fofa.UrlList)
+	checker := tools.NewChecker(&fofa.UrlList)
 	if *outPut != "" {
-		checker.OutPutFile=*outPut
+		checker.OutPutFile = *outPut
 	}
 	checker.StartCheck()
 }
-
